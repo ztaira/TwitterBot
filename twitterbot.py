994b0befb9859ea8392797904c868f6be6dff136
ztaira14 <ztaira14@punahou.edu>
2016-03-08 16:38:37 -0500

    Mentions Update 3/8
    Now has the ability to search and get mentions, as well as specific
    fields like who mention you or what they said.

--- twitterbot.py
+++ twitterbot.py
@@ -11,6 +11,7 @@ class TwitterBot():
                                self.creds[2], self.creds[3]))
         print("Logged in to twitter! :D")
         print("Executing programmed behavior...")
+        self.handle='@zachary_taira'
         self.behavior()
 
     def tweet2file(self, handle='@zachary_taira'):
@@ -62,6 +63,7 @@ class TwitterBot():
         if tweet[0]['text'] in filetext:
             return False
         else:
+            self.tweet2file()
             return True
 
     def poststatus(self, new_status="Hello World!"):
@@ -116,6 +118,63 @@ class TwitterBot():
                                                     include_rts=False)
         return tweet[0]
 
+    def mention2file(self):
+        """writes all mentions to file. Uses 1 API call."""
+        mentions = self.twitter.statuses.mentions_timeline()
+        file = open(self.handle+'m.txt', 'w+')
+        for mention in mentions:
+            file.write('\n(')
+            file.write(mention['text'])
+            file.write(')' + str(mention['user']['name']) + '\n')
+        file.close()
+
+    def getnewmention(self):
+        """Gets the latest mention. Uses 1 API call."""
+        mentions = self.twitter.statuses.mentions_timeline(count=1)
+        return mentions[0]
+
+    def getnewmentionuser(self):
+        """Gets the user who did the latest mention.""" 
+        mentions = self.twitter.statuses.mentions_timeline(count=1)
+        return mentions[0]['user']['name']
+
+    def getnewmentionhandle(self):
+        """Gets the handle of the user who did the latest mention."""
+        mentions = self.twitter.statuses.mentions_timeline(count=1)
+        return '@'+str(mentions[0]['user']['screen_name'])
+
+    def searchmentions(self, handle='@zachary_taira', sstring='Hello, world!',
+                     update=False):
+        """Searches a string against a given user's tweet log.
+        If update is true, updates tweet logs before checking.
+        Only uses 1 API call via self.mention2file"""
+        if update is True:
+            self.mention2file(handle)
+        file = open(handle+'m.txt', 'r')
+        filetext = file.read()
+        file.close()
+        if sstring in filetext:
+            return True
+        else:
+            return False
+
+    def newmention(self):
+        """Returns true if there's a new mention. Uses 1 API call."""
+        mention = self.getnewmention()
+        file = open(self.handle+'m.txt', 'r')
+        filetext = file.read()
+        file.close()
+        if mention['text'] in filetext:
+            return False
+        else:
+            self.mention2file()
+            return True
+
+    def getnewmentiontext(self):
+        """Gets the text of the latest mention."""
+        mentions = self.twitter.statuses.mentions_timeline(count=1)
+        return str(mentions[0]['text'])
+        
     def behavior(self):
         """This is what the twitterbot does when running"""
         # do stuff
